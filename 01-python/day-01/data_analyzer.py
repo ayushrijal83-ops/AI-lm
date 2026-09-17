db53419661a7b294274d0aae207191070c519cb0
ayushrijal83-ops <ayushrijal83@gmail.com>
2026-09-17 13:21:10 +0545

    first project completed

--- 01-python/day-01/data_analyzer.py
+++ 01-python/day-01/data_analyzer.py
@@ -1,67 +1,48 @@
 import csv as cs 
 with open("data.csv", "r") as file:
+
     reader = cs.reader(file)
     header = next(reader)
+
     highest_score = 0
-    highest_student = " "
+    highest_student = ""
     lowest_score = None
     lowest_student = ""
-    total_output = 0
     count = 0
-
-    #temp variable for sort time only
-    newname = ""
-    answer = 0
-
-
-   
-
-    # for row in reader:
-    #     score = int(row[2])
-    #     total_output = total_output + score
-    #     count = count + 1
-    # avrage = total_output / count
-    # print(avrage)
-
-    #for row in reader:
-      #  print(row)
-
-    
-
-    #task is to find the greatest score from all the students
-    
-    #this section is to find greatest score among the students get the mark and find who gets it highest
-    # for row in reader:
-    #     score = int(row[2])
-    #     if score > highest_score:
-    #         highest_score = score
-    #         highest_student = row[0]
-    #     count +=1
-
-
-    #this section is to find lowest score among the students get the mark and find who gets it lowest
-    # for row in reader:
-    #     score = int(row[2])
-    #     if lowest_score is None or score < lowest_score:
-    #         lowest_score = score
-    #         lowest_student = row[0]
-    #     count +=1
-
-    # print("Highest score = ", highest_score)
-    # print("Highest-scoring student = ", highest_student)
-    # print("Total students = ", count)
-
+    total_score = 0
+    high_scorers = []
 
     for row in reader:
-        if row in reader:
-            score = int(row[2])
-            if score >= 85:
-                print(row[0], score)
 
-            
+        score = int(row[2])
+        total_score = score + total_score
 
-    print("Lowest score = ", lowest_score)
-    print("lowest-scoring student = ", lowest_student)
-    print("Total students = ", count)
+        if score > highest_score:
+            highest_score = score
+            highest_student = row[0]
 
-    print(newname)
+        if lowest_score is None or score < lowest_score:
+            lowest_score = score
+            lowest_student = row[0]
+
+    
+        if score >= 85:
+            high_scorers.append((row[0], score))
+
+        count += 1
+    
+
+#Output part 
+print("----------------------STUDENT REPORT------------------------")
+print("Total Students = ", count)
+print("Avrage Score = ", total_score/count,"\n")
+
+print("Highest score = ",highest_score)
+print("Highest Student = ", highest_student,"\n")
+
+print("Lowest score = ", lowest_score)
+print("lowest student", lowest_student,"\n")
+
+print("<-------------Students getting 85 or higher:-------------->")
+for student, score in high_scorers:
+    print(student, score)
